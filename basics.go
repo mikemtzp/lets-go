dc8e250cb89f44632d3d66af4aee855a33938e1c
mikemtzp <mikemp8226@gmail.com>
2024-02-01 08:21:45 -0600

    add types, variables and functions concepts

--- basics.go
+++ basics.go
@@ -22,6 +22,20 @@ func varType() int {
   return count
 }
 
+// Type sizes
+// whole numbers: int  int8  int16  int32  int64 
+// positive whole numbers: uint uint8 uint16 uint32 uint64 uintptr 
+// decimal numbers: float32 float64 
+// imaginary numbers (rare): complex64 complex128
+
+// Types can be converted the following way
+func typeConvertion() int {
+  f := 88.26
+  n := int(f)
+
+  return n
+}
+
 // Type inference: the variable's type is inferred from the value on the right hand side.
 func typeInference() (int, int, float64, complex128) {
   var a int
@@ -35,6 +49,15 @@ func typeInference() (int, int, float64, complex128) {
 
   return b, i, f, c
 }
+
+// Same line declarations: we can declare multiple variables on the same line:
+func sameLineDecl() (int, string) {
+  order, command := 66, "Execute"
+
+  return order, command
+}
+
+
 
 // Constants
 //  Their value cannot change during the execution of the program.
@@ -53,4 +76,21 @@ func Hello(name string) string {
 // hi is a private function.
 func hi(name string) string {
   return "hi " + name
+}
+
+// When two or more consecutive named function parameters share a type, you can omit the type from all but the last.
+func add(x, y int) int {
+	return x + y
+}
+
+// A function can return any number of results.
+func results(x, y string) (string, string) {
+	return y, x
+}
+
+// A return statement without arguments returns the named return values. This is known as a "naked" return.
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
