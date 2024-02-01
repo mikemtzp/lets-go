53aab306c3eaaf5f2660a3a8d5bb8995a14eaee3
mikemtzp <mikemp8226@gmail.com>
2024-01-31 20:56:06 -0600

    add basics topic file

---
+++ basics.go
@@ -0,0 +1,41 @@
+package basics
+// Variables
+//  Can be defined by explicitly specifying a type.
+var explicit int // Explicitly typed
+
+// You can also use an initializer ':=', and the compiler will assign
+// the variable type to match the type of the initializer
+//  The return type(s) of a function must be specified in the function declaration.
+func implicit() int {
+  implicit := 10   // Implicitly typed as an int
+  return implicit
+}
+
+// Once declared, a variable's type can never change.
+func varType() int {
+  count := 1 // Assign initial value
+  count = 2  // Update to new value
+  
+  // count = false // This throws a compiler error due to assigning a non `int` type
+
+  return count
+}
+
+// Constants
+//  Their value cannot change during the execution of the program.
+const Age = 21 // Defines a numeric constant 'Age' with the value of 21
+
+// Functions
+// Go functions accept 0 or more parameters.
+// Parameters MUST BE explicitly typed, there is no type inference.
+// Functions with 'PascalCase' will be public and with 'camelCase' will be private.
+
+// Hello is a public function.
+func Hello(name string) string {
+  return hi(name)
+}
+
+// hi is a private function.
+func hi(name string) string {
+  return "hi " + name
+}
